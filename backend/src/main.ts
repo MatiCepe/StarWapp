ffedac729afb6ea2bf77b115dbe1174d88ea61c8
Mati Cepe <m.cepellotti@ensolvers.com>
2025-06-02 08:58:32 -0300

    changed port to test back deployment

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -10,6 +10,7 @@ async function bootstrap() {
     origin: process.env.FRONTEND_URL || 'http://localhost:3001',
     credentials: true,
   });
-  await app.listen(process.env.PORT ?? 3000);
+  const port = process.env.PORT || 3000;
+  await app.listen(port, '0.0.0.0');
 }
 bootstrap();
